4fb8712514b464f3972a3ffcfbee0e3618ccce0f
yutu0k <yutu0k@sjtu.edu.cn>
2025-02-04 15:41:15 +0800

    basic fcns success

---
+++ notify.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+
+import requests
+import os
+
+GOTIFY_APP_TOKEN = os.getenv("APP_TOKEN")
+GOTIFY_INSTANCE_URL = os.getenv("GOTIFY_INSTANCE_URL")
+
+json = {
+	"title": "Md Test",
+	"message": "Hello: ![](https://gotify.net/img/logo.png)",
+	"priority": "5",	
+	"extras": {
+		"client::display": {
+			"contentType": "text/markdown"
+		}
+	}
+}
+
+url = f"{GOTIFY_INSTANCE_URL}/message?token={GOTIFY_APP_TOKEN}"
+
+response = requests.post(url, json=json)
+
+# "extras":
+# {
+# 	"client::display": 
+# 	{
+# 		"contentType": "text/markdown"
+# 	}
+# }
