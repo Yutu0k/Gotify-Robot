2f41e4b33c8a45d98aca56851d8d6564ab88696b
yutu0k <yutu0k@sjtu.edu.cn>
2025-02-04 17:33:23 +0800

    markdown test success

--- notify.py
+++ notify.py
@@ -6,20 +6,26 @@ import os
 GOTIFY_APP_TOKEN = os.getenv("APP_TOKEN")
 GOTIFY_INSTANCE_URL = os.getenv("INSTANCE_URL")
 
+
 json = {
-	"title": "Md Test",
-	"message": "Hello: ![](https://gotify.net/img/logo.png)",
-	"priority": "5",	
+	"message": "popo提醒吃药药![蛙蛙](https://www.yutu0k.top/posts/11d3c1f7/image.png)。",
+	"priority": 5,
+	"title": "吃药药机器人发来消息",
 	"extras": {
 		"client::display": {
 			"contentType": "text/markdown"
 		}
 	}
+
 }
 
 url = f"{GOTIFY_INSTANCE_URL}/message?token={GOTIFY_APP_TOKEN}"
 
+# print(url)
+
 response = requests.post(url, json=json)
+
+print(response)
 
 # "extras":
 # {
